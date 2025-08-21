bb94457c3f7f27cb55c870f62d0e747f8111196a
Dimas Yusuf Qurohman <96605743+dimmasyusuf@users.noreply.github.com>
2025-08-21 13:11:58 +0700

    chore: setup shadcn ui component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center gap-10 p-24">
       <h1 className="text-4xl font-bold">Amara</h1>
+      <Button>Click me</Button>
     </main>
   );
 }
